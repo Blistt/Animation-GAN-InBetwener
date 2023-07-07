e141c9cd74673a134d09f2b4a89630209fba25e9
Blistt <farriagapazos@miners.utep.edu>
2023-07-06 23:00:55 -0600

    fucking finally works

--- train.py
+++ train.py
@@ -2,19 +2,20 @@ import torch
 from torch import nn
 from tqdm.auto import tqdm
 from torchvision import transforms
-from torchvision.utils import make_grid
 from torch.utils.data import DataLoader
-import matplotlib.pyplot as plt
-import torch.nn.functional as F
 from generator_light import GeneratorLight
 from discriminator import Discriminator
-from utils import weights_init
+from utils.utils import weights_init, visualize_batch
 from dataset_class import MyDataset
 from loss import get_gen_loss, gdl_loss, MS_SSIM
+from test import test
+import os
+from torchvision.utils import save_image
 
 
 def train(tra_dataset, gen, disc, gen_opt, disc_opt, adv_l, adv_lambda, l1=nn.L1Loss(), l2=None, l3=None, 
-          lamb1=100, lamb2=100, lamb3=100, n_epochs=10, batch_size=12, device='cuda:1', test_dataset=None, experiment_dir='exp/'):  
+          lamb1=100, lamb2=100, lamb3=100, n_epochs=10, batch_size=12, device='cuda:1', display_step=20,
+          test_dataset=None, my_dataset=None, experiment_dir='exp/'):  
     
     # stores generator losses
     tr_gen_losses = []  
@@ -44,7 +45,7 @@ def train(tra_dataset, gen, disc, gen_opt, disc_opt, adv_l, adv_lambda, l1=nn.L1
             gen_opt.zero_grad()
             preds = gen(input1, input2)
             gen_loss = get_gen_loss(preds, disc, real, adv_l, adv_lambda, l1=l1, l2=l2, l3=l3, 
-                                    lamb1=lam1, lamb2=lam2, lamb3=lam3)
+                                    lamb1=lamb1, lamb2=lamb2, lamb3=lamb3)
             gen_loss.backward()
             gen_opt.step()
 
@@ -76,50 +77,112 @@ def train(tra_dataset, gen, disc, gen_opt, disc_opt, adv_l, adv_lambda, l1=nn.L1
             args = locals()
             # Evaluate the model on the test dataset
             with torch.no_grad():
-                test_gen_loss, test_disc_loss = test(gen, disc, adv_l, adv_lambda, l1=l1, l2=l2, l3=l3, 
-                                                     lamb1=lamb1, lamb2=lamb2, lamb3=lamb3, batch_size=batch_size, device='cuda:0',
-                                                     test_dataset=test_dataset, experiment_dir=experiment_dir)
+                test_gen_loss, test_disc_loss = test(test_dataset, gen, disc, adv_l, adv_lambda, l1=l1, l2=l2, l3=l3,
+                                                     lamb1=lamb1, lamb2=lamb2, lamb3=lamb3, batch_size=batch_size, device=device,
+                                                     experiment_dir=experiment_dir)
             test_gen_losses.append(test_gen_loss)
             test_disc_losses.append(test_disc_loss)
+
+        
+        '''
+        Saves checkpoints, visualizes predictions and plots losses
+        '''
+        if epoch % display_step == 0:
+            # Save snapshot of model architecture``
+            if epoch == 0:
+                with open(experiment_dir + 'gen_architecture.txt', 'w') as f:
+                    print(gen, file=f)
+                with open(experiment_dir + 'disc_architecture.txt', 'w') as f:
+                    print(disc, file=f)
+            '''
+            Performs testing in MY dataset if specified
+            '''
+            if my_dataset is not None:
+                # Free up unused memory before starting testing process
+                torch.cuda.empty_cache()
+                gen.eval(), disc.eval()     # Set the model to evaluation mode              
+                # Evaluate the model on the MY dataset
+                with torch.no_grad():
+                    unused_loss = test(my_dataset, gen, disc, adv_l, adv_lambda, l1=l1, l2=l2, l3=l3, lamb1=lamb1, lamb2=lamb2, lamb3=lamb3, 
+                                       batch_size=batch_size, device=device, experiment_dir=experiment_dir)
+
+
+            '''MAIN VISUALIZATION BLOCK - Visualizes training predictions and plots training and testing losses'''
+            visualize_batch(input1, real, input2, preds, epoch, experiment_dir=experiment_dir, train_gen_losses=tr_gen_losses,
+                            train_disc_losses=tr_disc_losses, test_gen_losses=test_gen_losses, test_disc_losses=test_disc_losses,
+                            train_test='training')
+
+            # Saves checkpoing with model's current state
+            torch.save(gen.state_dict(), experiment_dir + 'gen_checkpoint' + str(epoch) + '.pth')
+            torch.save(disc.state_dict(), experiment_dir + 'disc_checkpoint' + str(epoch) + '.pth')
+                    
+        print(f"Epoch {epoch}: Training Gen loss: {tr_gen_losses[-1]} Training Disc loss: {tr_disc_losses[-1]} \
+              Testing Gen loss: {test_gen_losses[-1]} Testing Disc loss: {test_disc_losses[-1]}")
+        
+        # Keeps a log of the training and testing losses
+        with open(experiment_dir + 'training_log.txt', 'a') as f:
+            print(f"Epoch {epoch}: Training Gen loss: {tr_gen_losses[-1]} Training Disc loss: {tr_disc_losses[-1]} \
+                  Testing Gen loss: {test_gen_losses[-1]} Testing Disc loss: {test_disc_losses[-1]}", file=f)
+                        
+
             
-            
+
 if __name__ == '__main__':
+    
+    device = 'cuda:1'                   # Device (cuda or cpu)
+
     '''Loss function parameters'''
-    adv_l = nn.BCEWithLogitsLoss()
-    recon_l = nn.L1Loss()
-    gdl_l = gdl_loss
-    ms_ssim_l = MS_SSIM()
-    adv_lambda = 200
-    recon_lambda = 200
-    gdl_lambda = 200
-    ms_ssim_lambda = 200
+    adv_l = nn.BCEWithLogitsLoss()      # Adversarial loss
+    recon_l = nn.L1Loss()               # Reconstruction loss 1
+    gdl_l = gdl_loss                    # Reconstruction loss 2
+    ms_ssim_l = MS_SSIM(device)         # Reconstruction loss 3
+    adv_lambda = 0.05                   # Adversarial loss weight
+    recon_lambda = 1.0                  # Reconstruction loss 1 weight        
+    gdl_lambda = 1.0                    # Reconstruction loss 2 weight
+    ms_ssim_lambda = 6.0                # Reconstruction loss 3 weight    
 
     '''Training loop parameters'''
-    n_epochs = 20
-    input_dim = 3
-    real_dim = 3
-    display_step = 200
-    batch_size = 4
-    lr = 0.0002
-    target_shape = 256
-    device = 'cuda'
+    n_epochs = 20                       # Number of epochs
+    input_dim = 1                       # Input channels
+    hidden_channels = 64                # Hidden channels of the generator and discriminator
+    display_step = 1                    # How often to display/visualize the images
+    batch_size = 4                      # Batch size
+    lr = 0.0002                         # Learning rate
+    b1 = 0.5                            # Adam: decay of first order momentum of gradient
+    b2 = 0.999                          # Adam: decay of second order momentum of gradient
+    img_size = 512                      # Frames' image size
 
-    '''Dataset parameters'''
-    transform = transforms.Compose([transforms.ToTensor(),])
-    # Training dataset
-    training_data_dir = "data/frames"
-    tra_dataset = MyDataset(training_data_dir, transform=transform)
-
-    # Testing dataset (optional)
-    testing_data_dir = "data/frames"
-    test_dataset = MyDataset(testing_data_dir, transform=transform)
 
     '''Model parameters'''
-    gen = GeneratorLight(input_dim, real_dim).to(device)
-    gen_opt = torch.optim.Adam(gen.parameters(), lr=lr)
-    disc = Discriminator(input_dim + real_dim).to(device)
-    disc_opt = torch.optim.Adam(disc.parameters(), lr=lr)
+    gen = GeneratorLight(input_dim, hidden_channels).to(device)
+    gen_opt = torch.optim.Adam(gen.parameters(), lr=lr, betas=(b1, b2))
+    disc = Discriminator(input_dim, hidden_channels).to(device)
+    disc_opt = torch.optim.Adam(disc.parameters(), lr=lr, betas=(b1, b2))
 
+
+    '''Dataset parameters'''
+    device = 'cuda:1'
+    transform = transforms.Compose([
+        transforms.Resize((img_size, img_size)),
+        transforms.ToTensor(),])
+    # Training dataset
+    train_data_dir = 'mini_datasets/mini_train_triplets/'
+    # training_data_dir = '/data/farriaga/atd_12k/Line_Art/train_10k/'
+    train_dataset = MyDataset(train_data_dir, transform=transform)
+    # Testing dataset (optional)
+    test_data_dir = 'mini_datasets/mini_test_triplets/'
+    # training_data_dir = '/data/farriaga/atd_12k/Line_Art/test_2k_original/'
+    test_dataset = MyDataset(test_data_dir, transform=transform)
+    # MY dataset (optional)
+    my_data_dir = 'mini_datasets/mini_real_test_triplets/'
+    my_dataset = MyDataset(my_data_dir, transform=transform)
+
+    '''
+    Visualization parameters
+    '''
+    display_step = 10
+    experiment_dir = 'exp0/'
+    if not os.path.exists(experiment_dir): os.makedirs(experiment_dir)
 
     # Loads pre-trained model if specified
     pretrained = False
@@ -133,4 +196,6 @@ if __name__ == '__main__':
         gen = gen.apply(weights_init)
         disc = disc.apply(weights_init)
 
-train()
+    train(train_dataset, gen, disc, gen_opt, disc_opt, adv_l, adv_lambda, l1=recon_l, l2=gdl_l, l3=ms_ssim_l, lamb1=recon_lambda,
+          lamb2=gdl_lambda, lamb3=ms_ssim_lambda, n_epochs=10, batch_size=6, device=device, display_step=display_step,
+          test_dataset=test_dataset, my_dataset=my_dataset, experiment_dir=experiment_dir)
---
+++ utils/utils.py
@@ -0,0 +1,135 @@
+import numpy as np
+import math
+import cv2
+
+import torchvision.transforms as transforms
+import torch.nn.functional as F
+from PIL import Image
+from torch import nn
+import matplotlib.pyplot as plt
+from torchvision.utils import make_grid
+import os
+
+
+# import utils.calculator as cal
+
+def create_gif(input1, labels, input2, pred, experiment_dir, epoch):
+    pred = Image.fromarray((pred.detach().cpu().numpy().squeeze() * 255).astype(np.uint8))
+    input1 = Image.fromarray((input1.detach().cpu().numpy().squeeze() * 255).astype(np.uint8))
+    input2 = Image.fromarray((input2.detach().cpu().numpy().squeeze() * 255).astype(np.uint8))
+    labels = Image.fromarray((labels.detach().cpu().numpy().squeeze() * 255).astype(np.uint8))
+    
+    # Gif for generated triplet
+    input1.save(experiment_dir + 'triplet_' + str(epoch) + 'true_.gif', save_all=True, append_images=[labels, input2], duration=500, loop=0)
+    # Gif for ground truth triplet
+    input1.save(experiment_dir + 'triplet_' + str(epoch) + 'pred_.gif', save_all=True, append_images=[pred, input2], duration=500, loop=0)
+
+def weights_init(m):
+    if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
+        nn.init.normal_(m.weight, 0.0, 0.02)
+    if isinstance(m, nn.BatchNorm2d):
+        nn.init.normal_(m.weight, 0.0, 0.02)
+        nn.init.constant_(m.bias, 0)
+
+
+def show_tensor_images(image_tensor, num_images=16):
+    '''
+    Function for visualizing images: Given a tensor of images, number of images, and
+    size per image, plots and prints the images in an uniform grid.
+    '''
+    image_unflat = image_tensor.detach().cpu()
+    image_grid = make_grid(image_unflat[:num_images], nrow=4)
+    plt.axis('off')
+    plt.imshow(image_grid.permute(1, 2, 0).squeeze())
+
+
+def create_gif(input1, labels, input2, pred, experiment_dir, epoch):
+    pred = Image.fromarray(np.squeeze((pred[0].detach().cpu().numpy() * 255), axis=0))
+    input1 = Image.fromarray(np.squeeze((input1[0].detach().cpu().numpy() * 255), axis=0))
+    input2 = Image.fromarray(np.squeeze((input2[0].detach().cpu().numpy() * 255), axis=0))
+    labels = Image.fromarray(np.squeeze((labels[0].detach().cpu().numpy() * 255), axis=0))
+    # Gif for generated triplet
+    input1.save(experiment_dir + 'triplet_' + str(epoch) + 'true_.gif', save_all=True, append_images=[labels, input2], duration=500, loop=0)
+    # Gif for ground truth triplet
+    input1.save(experiment_dir + 'triplet_' + str(epoch) + 'pred_.gif', save_all=True, append_images=[pred, input2], duration=500, loop=0)
+
+
+def visualize_batch(input1, labels, input2, pred, epoch, experiment_dir='exp/', train_gen_losses=None, train_disc_losses=None,
+                    test_gen_losses=None, test_disc_losses=None, train_test='training', figsize=(20,10)):
+        
+        # Creates experiment directory if it doesn't exist
+        experiment_dir = experiment_dir + train_test + '/'
+        if not os.path.exists(experiment_dir): os.makedirs(experiment_dir)
+
+        if train_gen_losses is not None and test_gen_losses is not None:
+            # Plots Generator and Discriminator losses in the same plot
+            plt.figure()
+            plt.subplot(1,2,1)
+            plt.plot(train_gen_losses, label='Generetor')
+            plt.plot(train_disc_losses, label='Discriminator')
+            plt.title("Training Loss per Epoch")
+            plt.xlabel("Epoch")
+            plt.ylabel("Loss")
+            plt.subplot(1,2,2)
+            plt.plot(test_gen_losses, label='Generetor')
+            plt.plot(test_disc_losses, label='Discriminator')
+            plt.title("Testing Loss per Epoch")
+            plt.xlabel("Epoch")
+            plt.ylabel("Loss")
+            plt.legend()
+            plt.savefig(experiment_dir + 'loss' + str(epoch) + '.png')
+
+            # Plots generated images
+            plt.figure(figsize=figsize) # Create a new figure for the subplots
+            plt.subplot(1,2,1)
+            show_tensor_images(labels, num_images=labels.shape[0])
+            plt.title("True")
+            plt.subplot(1,2,2)
+            show_tensor_images(pred, num_images=pred.shape[0])
+            plt.title("Generated")
+            plt.savefig(experiment_dir + 'gens' + str(epoch) + '.png')
+
+            # Saves gifs of the predicted and ground truth triplets
+            create_gif(input1, labels, input2, pred, experiment_dir, epoch)
+        
+        if train_gen_losses is None and test_gen_losses is not None:
+            # Plots generated images
+            plt.figure(figsize=figsize) # Create a new figure for the subplots
+            plt.subplot(1,2,1)
+            show_tensor_images(labels)
+            plt.title("True")
+            plt.subplot(1,2,2)
+            show_tensor_images(pred)
+            plt.title("Generated")
+            plt.savefig(experiment_dir + 'gens' + str(epoch) + '.png')
+
+            # Saves gifs of the predicted and ground truth triplets
+            create_gif(input1, labels, input2, pred, experiment_dir, epoch)
+
+        '''PLOTS ONLY TRAINING IF NO TESTING IS CALLED'''
+        if train_gen_losses is not None and test_gen_losses is None:
+            # Plots Generator and Discriminator losses in the same plot
+            plt.figure()
+            plt.plot(train_gen_losses, label='Training')
+            plt.title("Loss per Epoch")
+            plt.xlabel("Epoch")
+            plt.ylabel("Loss")
+            plt.legend()
+            plt.savefig(experiment_dir + 'loss' + str(epoch) + '.png')
+
+            # Plots generated images
+            plt.figure(figsize=figsize)
+
+            # Plots generated images
+            plt.figure(figsize=figsize) # Create a new figure for the subplots
+            plt.subplot(1,2,1)
+            show_tensor_images(labels)
+            plt.title("True")
+            plt.subplot(1,2,2)
+            show_tensor_images(pred)
+            plt.title("Generated")
+            plt.savefig(experiment_dir + 'gens' + str(epoch) + '.png')
+
+            # Saves gifs of the predicted and ground truth triplets
+            create_gif(input1, labels, input2, pred, experiment_dir, epoch)
+        
