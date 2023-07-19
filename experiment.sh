18db0f62a05862cd42ef1e4f64525835b9832176
Blistt <farriagapazos@miners.utep.edu>
2023-07-19 16:39:03 -0600

    training loop was broken, fixed it and run it agai

--- experiment.sh
+++ experiment.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+python exp_crop4.py
 python exp_crop4.5.py
+python exp_crop5.py
 python exp_crop6.py
 python exp_crop7.py
 python exp_crop4_all.py
 python exp_crop5_all.py
+python exp_crop6_all.py
 python exp_crop7_all.py
 python exp_light.py
