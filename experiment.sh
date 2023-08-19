f43285006dc908b1846d629f5142fa881a0f7e57
Blistt <farriagapazos@miners.utep.edu>
2023-08-19 00:37:12 -0600

    running epochs and new steps experiments

--- experiment.sh
+++ experiment.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
-python /home/farriaga/gan-interpolator/run_experimentt.py --config /home/farriaga/gan-interpolator/exp_crop1_epochs.ini
+python /home/farriaga/gan-interpolator/run_experiment.py --config /home/farriaga/gan-interpolator/exp_crop13_steps.ini
+python /home/farriaga/gan-interpolator/run_experiment.py --config /home/farriaga/gan-interpolator/exp_crop15_steps.ini
+python /home/farriaga/gan-interpolator/run_experiment.py --config /home/farriaga/gan-interpolator/exp_crop16_steps.ini
+python /home/farriaga/gan-interpolator/run_experiment.py --config /home/farriaga/gan-interpolator/exp_crop17_steps.ini
+python /home/farriaga/gan-interpolator/run_experiment.py --config /home/farriaga/gan-interpolator/exp_crop18_steps.ini
+python /home/farriaga/gan-interpolator/run_experiment.py --config /home/farriaga/gan-interpolator/exp_crop19_steps.ini
+
+python /home/farriaga/gan-interpolator/run_experiment.py --config /home/farriaga/gan-interpolator/exp_crop_base_epochs.ini
+python /home/farriaga/gan-interpolator/run_experiment.py --config /home/farriaga/gan-interpolator/exp_crop1_epochs.ini
+python /home/farriaga/gan-interpolator/run_experiment.py --config /home/farriaga/gan-interpolator/exp_crop7_epochs.ini
+python /home/farriaga/gan-interpolator/run_experiment.py --config /home/farriaga/gan-interpolator/exp_crop8_epochs.ini
+python /home/farriaga/gan-interpolator/run_experiment.py --config /home/farriaga/gan-interpolator/exp_crop9_epochs.ini
+python /home/farriaga/gan-interpolator/run_experiment.py --config /home/farriaga/gan-interpolator/exp_crop11_epochs.ini
+python /home/farriaga/gan-interpolator/run_experiment.py --config /home/farriaga/gan-interpolator/exp_light_base_epochs.ini
+python /home/farriaga/gan-interpolator/run_experiment.py --config /home/farriaga/gan-interpolator/exp_light1_epochs.ini
+python /home/farriaga/gan-interpolator/run_experiment.py --config /home/farriaga/gan-interpolator/exp_light2_epochs.ini
+
+python /home/farriaga/gan-interpolator/run_experiment.py --config /home/farriaga/gan-interpolator/exp20_steps.ini
