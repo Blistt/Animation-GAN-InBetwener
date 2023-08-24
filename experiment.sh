7c0affe42986f04bd7b7ce30a8450fc1cd3fd02d
Blistt <farriagapazos@miners.utep.edu>
2023-08-24 12:36:18 -0600

    doing long experiments

--- experiment.sh
+++ experiment.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-python /home/farriaga/gan-interpolator/run_experiment.py --config /home/farriaga/gan-interpolator/exp20_steps.ini
-
+python /home/farriaga/gan-interpolator/run_experiment.py --config /home/farriaga/gan-interpolator/exp_crop22_steps.ini
+python /home/farriaga/gan-interpolator/run_experiment.py --config /home/farriaga/gan-interpolator/exp_crop23_steps.ini
+python /home/farriaga/gan-interpolator/run_experiment.py --config /home/farriaga/gan-interpolator/exp_crop24_steps.ini
+python /home/farriaga/gan-interpolator/run_experiment.py --config /home/farriaga/gan-interpolator/exp_crop25_steps.ini
+python /home/farriaga/gan-interpolator/run_experiment.py --config /home/farriaga/gan-interpolator/exp_crop26_steps.ini
+python /home/farriaga/gan-interpolator/run_experiment.py --config /home/farriaga/gan-interpolator/exp_crop27_steps.ini
