59dbafb40d7ec915b91d76ada2afdea2471918c8
Blistt <farriagapazos@miners.utep.edu>
2023-08-26 17:32:31 -0600

    big experiments done about to try siren activation

--- experiment.sh
+++ experiment.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-python /home/farriaga/gan-interpolator/run_experiment.py --config /home/farriaga/gan-interpolator/exp_crop22_steps.ini
-python /home/farriaga/gan-interpolator/run_experiment.py --config /home/farriaga/gan-interpolator/exp_crop23_steps.ini
-python /home/farriaga/gan-interpolator/run_experiment.py --config /home/farriaga/gan-interpolator/exp_crop24_steps.ini
-python /home/farriaga/gan-interpolator/run_experiment.py --config /home/farriaga/gan-interpolator/exp_crop25_steps.ini
-python /home/farriaga/gan-interpolator/run_experiment.py --config /home/farriaga/gan-interpolator/exp_crop26_steps.ini
+python /home/farriaga/gan-interpolator/run_experiment.py --config /home/farriaga/gan-interpolator/exp_crop_base_steps_2.ini
+python /home/farriaga/gan-interpolator/run_experiment.py --config /home/farriaga/gan-interpolator/exp_light_base_steps_2.ini
+python /home/farriaga/gan-interpolator/run_experiment.py --config /home/farriaga/gan-interpolator/exp_light1_steps_2.ini
 python /home/farriaga/gan-interpolator/run_experiment.py --config /home/farriaga/gan-interpolator/exp_crop27_steps.ini
