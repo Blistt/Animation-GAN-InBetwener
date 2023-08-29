0a065103349a972445d8856a4a464ab83b2809c7
Blistt <farriagapazos@miners.utep.edu>
2023-08-28 23:25:41 -0600

    failed big experiments (siren model no siren ini)

--- experiment.sh
+++ experiment.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 # python /home/farriaga/gan-interpolator/run_experiment.py --config /home/farriaga/gan-interpolator/exp_light_base_steps_siren.ini
 # python /home/farriaga/gan-interpolator/run_experiment.py --config /home/farriaga/gan-interpolator/exp_crop_base_steps_siren.ini
-python /home/farriaga/gan-interpolator/run_experiment.py --config /home/farriaga/gan-interpolator/exp_crop27_steps_siren.ini
+python /home/farriaga/gan-interpolator/run_experiment.py --config /home/farriaga/gan-interpolator/exp_light3_steps.ini
+python /home/farriaga/gan-interpolator/run_experiment.py --config /home/farriaga/gan-interpolator/exp_light4_steps.ini
+python /home/farriaga/gan-interpolator/run_experiment.py --config /home/farriaga/gan-interpolator/exp_light5_steps.ini
+python /home/farriaga/gan-interpolator/run_experiment.py --config /home/farriaga/gan-interpolator/exp_light_base_steps_3.ini
+python /home/farriaga/gan-interpolator/run_experiment.py --config /home/farriaga/gan-interpolator/exp_crop28_steps.ini
+python /home/farriaga/gan-interpolator/run_experiment.py --config /home/farriaga/gan-interpolator/exp_crop27_steps.ini
