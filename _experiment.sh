e78899bea835b5138bb4cce8a4ffab25c31b0e91
Blistt <farriagapazos@miners.utep.edu>
2023-09-13 00:05:54 -0600

    about to run experiments with big dataset

--- _experiment.sh
+++ _experiment.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-python /home/farriaga/gan-interpolator/run_experiment.py --config /home/farriaga/gan-interpolator/exp_light8_steps_spectral.ini
-python /home/farriaga/gan-interpolator/run_experiment.py --config /home/farriaga/gan-interpolator/exp_light9_steps.ini
+python /home/farriaga/gan-interpolator/_run_experiment.py --config /home/farriaga/gan-interpolator/exp_light8_steps_spectral.ini
+````````````
